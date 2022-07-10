aaf109a2e5586a3e79a4cee035b3794ac48945ec
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2022-07-10 21:23:28 +0700

    create FK playlist_activities to playlists

---
+++ migrations/1657462569404_create-foreign-key-playlist-activities-to-playlists.js
@@ -0,0 +1,7 @@
+exports.up = (pgm) => {
+  pgm.addConstraint('playlist_activities', 'fk_playlist_activities.playlist_id_playlist.id', 'FOREIGN KEY (playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('playlist_activities', 'fk_playlist_activities.playlist_id_playlist.id', 'FOREIGN KEY (playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
+};
